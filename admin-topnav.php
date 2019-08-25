81c0ca774185907cdab1524b0f7ec4ff9e2e6649
RaIhAn <raihanrko007@gmail.com>
2019-08-25 18:32:41 +0600

    admin-topnav.php file are edited

--- admin-topnav.php
+++ admin-topnav.php
@@ -1,61 +1,105 @@
-<nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+<?php
+if ($_SESSION['user_type'] == 0) {
+    ?>
+    <nav class="navbar navbar-expand navbar-dark bg-success static-top">
 
-    <a class="navbar-brand mr-1" href="admin-dashboard.php">TutorialNow</a>
+        <a class="navbar-brand mr-1"
+           href="<?php echo ($_SESSION['user_type'] == 0) ? 'admin-dashboard.php' : 'home.php' ?>">TutorialNow</a>
 
-    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-        <i class="fas fa-bars"></i>
-    </button>
+        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+            <i class="fas fa-bars"></i>
+        </button>
 
-    <!-- Navbar Search -->
-    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for..." aria-label="Search"
-                   aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search"></i>
-                </button>
-            </div>
+        <!-- Navbar Search -->
+        <div class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <!--<div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for..." aria-label="Search"
+                       aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>-->
         </div>
-    </form>
 
-    <!-- Navbar -->
-    <ul class="navbar-nav ml-auto ml-md-0">
-        <!--<li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-bell fa-fw"></i>
-                <span class="badge badge-danger">9+</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>
-        <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <span class="badge badge-danger">7</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>-->
-        <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-               aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-user-circle fa-fw"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">Settings</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-            </div>
-        </li>
-    </ul>
+        <!-- Navbar -->
+        <ul class="navbar-nav ml-auto ml-md-0">
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle fa-fw"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="profile.php">Profile</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                </div>
+            </li>
+        </ul>
 
-</nav>
+    </nav>
+    <?php
+} elseif ($_SESSION['user_type'] == 1) {
+    ?>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+
+        <h5 class="my-0 mr-md-auto font-weight-normal">
+            <a class="navbar-brand" href="home.php">
+                <i class="fas fa-graduation-cap"></i>
+                TutorialNow
+            </a>
+        </h5>
+
+        <nav class="my-2 my-md-0 mr-md-3">
+            <div class="p-2 dropdown">
+                <a class="btn btn-light dropdown-toggle" href="#" role="button" id="categoryDropdownLink"
+                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="categoryDropdownLink">
+                    <?php
+                    $show_category = "select * from video_category";
+                    $result = $conn->query($show_category);
+                    if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                    ?>
+                    <li>
+                        <a class="text-dark dropdown-item" href="categories.php?id=<?php echo $row['category_id'] ?>">
+                            <?php echo $row['category_name'] ?>
+                        </a>
+                    <li>
+                        <?php
+                        }
+                        }
+                        ?>
+                </div>
+            </div>
+            <ul class="dropdown-menu">
+                <?php
+                $show_category = "select * from video_category";
+                $result = $conn->query($show_category);
+                if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                ?>
+                <li>
+                    <a class="p-2 text-dark dropdown-item" href="categories.php?id=<?php echo $row['category_id'] ?>">
+                        <?php echo $row['category_name'] ?>
+                    </a>
+                <li>
+                    <?php
+                    }
+                    }
+                    ?>
+            </ul>
+            <!--        <a class="p-2 text-dark" href="#">Link 3</a>-->
+            <!--        <a class="p-2 text-dark" href="#">Link 3</a>-->
+        </nav>
+        <a class="p-2 text-dark btn-link" href="profile.php"><i class="fas fa-user-astronaut"></i> Profile</a>
+        <a class="btn btn-outline-danger" href="logout.php">Logout</a>
+    </div>
+    <?php
+}
+?>
